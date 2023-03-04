003caff5bf2928b92952777f14c2dfe83920f65b
Vera Farinha <vera.farinha82@gmail.com>
2023-03-04 10:52:32 +0000

    update

---
+++ web/3 - Courses/B - Templates/templates/views/home.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from fastapi_chameleon import template
+
+router = APIRouter()
+
+@router.get('/')
+@template()
+async def index(course1: str = 'N/D'):
+    return{
+        'course1': course1,
+        'course2': 'Contabilidade'
+        'course3': 'Electrónica'
+    }
+#:
+
+@router.get('/about')
+@template()
+async def about():
+    return {
+        'nome': 'Alberto',
+    }
